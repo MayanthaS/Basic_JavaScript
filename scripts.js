6b61bc13b313926f3e6c0b33f22c914ae2aacf94
MayanthaS <sapumalmayantha@gmail.com>
2025-09-15 11:31:44 +0530

    added the internal and external js and create basic vaeiable

---
+++ scripts.js
@@ -0,0 +1,12 @@
+//----External----------//
+
+//print
+console.log("Hope you are fine")
+
+//create variable
+var firstName = "Chamara";
+
+
+//print
+console.log(firstName);
+console.log('My Name is ' +'' + firstName);
