573696e5b4e74ac6d02be68347e9f86ea4087a0c
MayanthaS <sapumalmayantha@gmail.com>
2025-09-15 12:02:41 +0530

    learn get variable type,varibale mutation, display alert, get user input

--- scripts.js
+++ scripts.js
@@ -12,5 +12,20 @@ var age =26;
 console.log(firstName);
 console.log('My Name is ' +'' + firstName);
 //type coerction
-console.log('My AGE IS' +age);
+console.log('My AGE IS ' +age);
 
+//get variable type
+console.log(typeof(firstName));
+
+//define 
+job ='Doctor';
+//variable mutation
+lastname ='jehan';
+console.log('I am '+firstName +'i am '+ age+ ' years old ');
+
+//display alert
+alert('HI ' +lastname );
+
+//Get User input
+Hegiht = prompt('What is your height?');
+alert('My height is ' +Hegiht +'feet');
