e994666faf29c703d1385be38008046777dca89c
MayanthaS <sapumalmayantha@gmail.com>
2025-09-15 11:39:57 +0530

    add type coertion

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 //----External----------//
 
 //print
-console.log("Hope you are fine")
+//console.log("Hope you are fine")
 
 //create variable
 var firstName = "Chamara";
+var age =26;
 
 
 //print
 console.log(firstName);
 console.log('My Name is ' +'' + firstName);
+//type coerction
+console.log('My AGE IS' +age);
+
