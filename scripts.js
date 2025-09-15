c37c7544b9d2cb620874782554a661ae0bbc8858
MayanthaS <sapumalmayantha@gmail.com>
2025-09-15 12:26:50 +0530

    learn arithmatic operation  in js

--- scripts.js
+++ scripts.js
@@ -9,23 +9,42 @@ var age =26;
 
 
 //print
-console.log(firstName);
-console.log('My Name is ' +'' + firstName);
+//console.log(firstName);
+//console.log('My Name is ' +'' + firstName);
 //type coerction
-console.log('My AGE IS ' +age);
+//console.log('My AGE IS ' +age);
 
 //get variable type
-console.log(typeof(firstName));
+//console.log(typeof(firstName));
 
 //define 
-job ='Doctor';
+//job ='Doctor';
 //variable mutation
-lastname ='jehan';
-console.log('I am '+firstName +'i am '+ age+ ' years old ');
+//lastname ='jehan';
+//console.log('I am '+firstName +'i am '+ age+ ' years old ');
 
 //display alert
-alert('HI ' +lastname );
+//alert('HI ' +lastname );
 
 //Get User input
-Hegiht = prompt('What is your height?');
-alert('My height is ' +Hegiht +'feet');
+//Hegiht = prompt('What is your height?');
+//alert('My height is ' +Hegiht +'feet');
+
+//--singel operato--
+var myAge,yourAge,year,result;
+myAge=26;
+yourAge=20;
+year =2025;
+
+//subtraction
+result = year-myAge;
+console.log("i was born in"+result);
+
+//addition 
+result = year+10;
+console.log('product of our ages ' + result);
+
+//multiplication 
+result= myAge * yourAge
+console.log("produt of age "+ result);
+
