591cfc68b70a95a303470f7c5c12f25d99978745
MayanthaS <sapumalmayantha@gmail.com>
2025-09-21 22:00:07 +0530

    relaenrning and create simple project subtraction and addition

--- scrips.js
+++ scrips.js
@@ -1,8 +1,18 @@
 //external js
 //print
+/*
 console.log("hi mayantha");
 alert("hi mayanatha hoe are yo?");
 
 //get data
 var name =prompt("what is your name?")
 alert('HI ' + name);
+*/
+// learning subtraction addtion 
+var name = prompt("Enter your name?");
+var age = prompt("Enter your age?");
+var bithyear = nowyear- age;
+var nowyear = prompt("what is this year?")
+console.log('your name is' +name);
+console.log('your age is ' +age);
+console.log("your birth year is "+bithyear);
