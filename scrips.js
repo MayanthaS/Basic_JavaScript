33bcc0a081520fae1a5ae5c2985d39b541996f71
MayanthaS <sapumalmayantha@gmail.com>
2025-09-22 11:10:38 +0530

    create simple fingerprint project

--- scrips.js
+++ scrips.js
@@ -9,10 +9,26 @@ var name =prompt("what is your name?")
 alert('HI ' + name);
 */
 // learning subtraction addtion 
+/*
 var name = prompt("Enter your name?");
 var age = prompt("Enter your age?");
-var bithyear = nowyear- age;
 var nowyear = prompt("what is this year?")
-console.log('your name is' +name);
+console.log('your name is ' +name);
 console.log('your age is ' +age);
+var bithyear ;
+bithyear  = nowyear - age;
 console.log("your birth year is "+bithyear);
+*/
+//finger print operation
+var name = prompt("Enter name :");
+var time = prompt("Enter time:");
+if(time<12.00){
+    alert("Good Morning "+name);
+}
+else if(time <15.00){
+    alert("Good Afternoon "+name);
+}
+else{
+    alert("Good Evening "+name);
+}
+
